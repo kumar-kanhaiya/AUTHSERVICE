732ef807ccceaf9f7805d69cb6aaf14129315442
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-12 17:46:20 +0530

    code

--- app/src/main/java/org/example/entities/RefreshToken.java
+++ app/src/main/java/org/example/entities/RefreshToken.java
@@ -1,5 +1,36 @@
 package org.example.entities;
 
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Table(name = "tokens")
 public class RefreshToken {
+
+    @Id // creating the id
+    @GeneratedValue(strategy = GenerationType.AUTO) // generate the automatic value
+    private int id;
+
+    private String token;
+
+    private Instant expireDate; // Instant is used to get the time and date
+
+    @OneToOne
+    @JoinColumn(name = "id" , referencedColumnName = "user_id")
+
+    private UserInfo userInfo;
 
 }
