6b6052e0be759d15b3fb6dbd4e545f8993e62c07
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-14 11:42:25 +0530

    codE

--- app/src/main/java/org/example/Service/JwtService.java
+++ app/src/main/java/org/example/Service/JwtService.java
@@ -2,13 +2,16 @@ package org.example.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -43,6 +46,24 @@ public class JwtService {
     private Key getSignKey(){
         byte[] keyBytes = Decoders.BASE64.decode(SECRET);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean validateToken(String token , UserDetails userDetails){
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private String createToken(Map<String , Object> claims , String username){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*1))
+                .signWith(getSignKey() , SignatureAlgorithm.HS256).compact();
     }
 
 }
